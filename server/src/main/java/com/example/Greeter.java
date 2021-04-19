9328537fa99f63b90d0d024379729ce79ce1079c
ZinaIbouroi <82593343+ZinaIbouroi@users.noreply.github.com>
2021-04-19 15:59:51 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,4 +16,7 @@ public class Greeter {
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
+   public final String greet(final String someone) {
+    return String.format("Hello babacar, %s!", someone);
+  }
 }
